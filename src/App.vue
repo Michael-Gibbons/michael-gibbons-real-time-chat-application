eb3624e95e90745f4085add0c258f5409fc2d9b7
Michael Gibbons <michael@btownweb.com>
2021-02-21 00:55:45 -0700

    Added tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/logo.png" class="m-auto">
   <HelloWorld msg="My first Vue app using cli"/>
 </template>
 
