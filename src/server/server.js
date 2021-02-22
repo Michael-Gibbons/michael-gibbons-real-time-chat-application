45cb4364e967f1638d510f0ea8048f1ea5869635
Michael Gibbons <michael@btownweb.com>
2021-02-21 21:30:53 -0700

    Fixed socket.io version mismatch and added cors exception

--- src/server/server.js
+++ src/server/server.js
@@ -3,10 +3,19 @@ const app = new Koa();
 const PORT = 8081 || process.env.PORT;
 
 var server = require('http').createServer(app.callback());
-var io = require('socket.io')(server);
+let CLIENT = 'http://localhost:8080';
+const io = require("socket.io")(server, {
+  cors: {
+    origin: CLIENT,
+    methods: ["GET", "POST"]
+  }
+});
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 server.listen(PORT, () => {
